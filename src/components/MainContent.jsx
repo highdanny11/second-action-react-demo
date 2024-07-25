49d44964e9b8017c20dc93a6e2a405a5e6754921
BL001 <>
2024-07-25 16:43:22 +0800

    測試發 pr

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -5,6 +5,7 @@ import HelpArea from './HelpArea';
 function MainContent() {
   const [helpVisible, setHelpVisible] = useState(false);
 
+  console.log('123')
   function toggleHelp() {
     setHelpVisible((isVisible) => !isVisible);
   }
